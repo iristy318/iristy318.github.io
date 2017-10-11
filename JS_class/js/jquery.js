1aaf8a69421849b18fdf50b0aa3835f98bb765c7
iristy318 <30558080+iristy318@users.noreply.github.com>
2017-10-11 18:09:42 -0400

    homework for 10/11

--- JS_class/js/jquery.js
+++ JS_class/js/jquery.js
@@ -1,10 +1,25 @@
+console.log('linked!');
+
 function choice(arr){
-  return arr[Math.floor(Math.random()*4)]
+  return arr[Math.floor(Math.random()*arr.length)]
 }
 
-var bg = ['images/1-dinosaur.gif','images/2.gif','images/1.gif','images/3.gif'];
+
+var bg = ['images/dinosaur.gif','images/1-dinosaur.gif','images/2.gif','images/1.gif','images/3.gif'];
 var currentbg = choice(bg);
 
-$('.images').attr('src',currentbg);
+//$('.images').attr('src',currentbg);
 
-console.log('linked!');
+
+var containers = ['container1','container2','container3'];
+var currentcontainer = choice(containers);
+
+var elems = $("div");
+if (elems.length) {
+  var keep = Math.floor(Math.random() * elems.length);
+  for (var i = 0; i < elems.length; ++i) {
+    if (i !== keep) {
+      $(elems[i]).hide();
+    }
+  }
+}
